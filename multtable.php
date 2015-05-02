7ca84d252e2673e3439d05a921dba41b75b827ff
gallinaj <gallinaj@onid.oregonstate.edu>
2015-05-02 10:59:04 -0700

    Finished writing multtable.php

--- multtable.php
+++ multtable.php
@@ -13,35 +13,81 @@ ini_set('display_errors', 'On');
 		extract($_GET);
 		
 		#Code assistance from lecture, 290 study session, 
+		#Check for missing values
 		if(!isset($min_multiplicand, $max_multiplicand, $min_multiplier, $max_multiplier)) {
 			echo "Missing parameter!";
 		}
 		else {
+			#Check parameters for values less than 0
 			if(!is_numeric($min_multiplicand) || $min_multiplicand < 0) {
 				echo  "min_multiplicand must be an integer greater than 0!<br />";
 			}
-			if(!is_numeric($max_multiplicand) || $max_multiplicand < 0) {
+			elseif(!is_numeric($max_multiplicand) || $max_multiplicand < 0) {
 				echo  "max_multiplicand must be an integer greater than 0!<br />";
 			}
-			if(!is_numeric($min_multiplier) || $min_multiplier < 0) {
+			elseif(!is_numeric($min_multiplier) || $min_multiplier < 0) {
 				echo  "min_multiplier must be an integer greater than 0!<br />";
 			}
-			if(!is_numeric($max_multiplier) || $max_multiplier < 0) {
+			elseif(!is_numeric($max_multiplier) || $max_multiplier < 0) {
 				echo  "min_multiplicand must be an integer greater than 0!<br />";
 			}
-			
-			if($min_multiplicand > $max_multiplicand) {
+			#Check parameters for max greater than min values
+			elseif($min_multiplicand > $max_multiplicand) {
 				echo "The minimum multiplicand is larger than the maximum.<br />";
 			}
-			if($min_multiplier > $max_multiplier) {
+			elseif($min_multiplier > $max_multiplier) {
 				echo "The minimum multiplier is larger than the maximum.<br />";
 			}
+			#Parameters are good; create table with them
 			else {
-				echo "Looks good, let's go!<br />";
+				#echo "Looks good, let's go!<br />";
+				
+				$rows = $max_multiplicand - $min_multiplicand + 2;
+				$cols = $max_multiplier - $min_multiplier + 2;
+				$initRowVal = $min_multiplicand;
+				$initColVal = $min_multiplier;
+				$rowVals = $min_multiplicand;
+				$colVals = $min_multiplier;
+
+				echo "<table border = 1px>";
+					echo "<thead>";
+					echo "<th>";
+					echo "</th>";
+						for($j = 0; $j < ($cols - 1); $j++) 
+						{
+							echo "<th>";
+							echo $min_multiplier;
+							echo "</th>";
+							$min_multiplier = $min_multiplier + 1;
+						}
+					echo "</thead>";
+					echo "<tbody>";
+						for($i = 0; $i < ($rows - 1); $i++) 
+						{
+							#$rowVals = $initRowVal;
+							$colVals = $initColVal;
+							echo "<tr>";
+							echo "<th>";
+							echo $min_multiplicand;
+							$min_multiplicand += 1;
+							echo "</th>";
+								
+							for($j = 0; $j < ($cols - 1); $j++) 
+							{
+								echo "<td>";
+								echo $rowVals * $colVals;
+								$colVals += 1;
+							}
+							$rowVals += 1;
+
+							echo "</tr>";
+						}					
+					echo "</tbody>";
+				echo "</table>";
+
+				
 			}
-		
-		
-		
+
 		}
 		
 
