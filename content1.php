05b93ac422fcaee99cf3e0172ac44495d28e4380
gallinaj <gallinaj@onid.oregonstate.edu>
2015-05-02 15:31:03 -0700

    Worked on loopback file

--- content1.php
+++ content1.php
@@ -2,8 +2,20 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 session_start();
-?>
 
+#Username was entered and the value is logout
+if(isset($_GET['action']) && $_GET['action'] == 'logout') {
+	#Empties session array
+	$_SESSION = array();
+	session_destroy();
+#	$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+#	$filePath = implode('/', $filePath);
+#	$redirect = "http://".$_SERVER['HTTP_HOST'].$filePath;
+#	header("location: {redirect}/login.php", true);
+	header("location: login.php");
+	die();
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -13,40 +25,32 @@ session_start();
 	<?php
 		#Starts a new session or resumes a previous one
 		#session_start();
-
+		
 		#No username was entered
 		if(!isset($_POST['username'])) {
 			echo "A username must be entered. Click <a href=\"login.php\">here</a> ";
-			echo " to return to the login screen.";
+			echo " to return to the login screen.<br />";
 		}	
-		#Username was entered and the value is end
-#		if(isset($_POST['username']) && $_POST['username'] == 'end') {
-			#Empties session array
-#			$_SESSION = array();
-#			session_destroy;
-#			$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
-#			$filePath = implode('/', $filePath);
-#			$redirect = "http://".$_SERVER['HTTP_HOST'].$filePath;
-#			header("Location: {redirect}login.php", true);
-#			die();
-#		}
-		
 		if(session_status() == PHP_SESSION_ACTIVE) {
+			
+			
 			#Write to session array
 			#Username was entered
 			if(isset($_POST['username'])) {
 				$_SESSION['username'] = $_POST['username'];
 			}
+
 			#User has not visited session before
 			if(!isset($_SESSION['visits'])) {
 				$_SESSION['visits'] = 0;
 			}
+
 			#Read from session array
 			echo "Hello ".$_SESSION['username'].". You have visited this page ".$_SESSION['visits']." times ";
 			echo "before.<br />";
 			echo "Click <a href=\"content2.php\">here</a> for the next page.<br />";
 			
-			echo "Click <a href=\"logout.php\">here</a> to logout.";
+			echo "Click <a href=\"content1.php?action=logout\">here</a> to logout.";
 
 			$_SESSION['visits']++;
 			}
--- loopback.php
+++ loopback.php
@@ -2,15 +2,55 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Madlibs!</title>
-		<link rel="stylesheet" type="text/css" href="madlib.css">
+		<title>Loopback</title>
 	</head>
 	<body>
 		<?php
+
+		#Code assistance from http://stackoverflow.com/questions/8140915/how-to-handle-unknown-number-of-items-from-a-form-in-php
+#		$a = array('foo' => 'bar', 'baz' => 'long');
+#		echo "<p>";
+#		echo json_encode($a);
+#		echo "</p>";
+		
+ 		if(isset($_GET)) {
+#			echo json_encode($_GET), "\n";
+
+			echo "{\"Type\":\"[GET]\"";
+
+			foreach($_GET as $key => $value) {
+				
+				if($value == null) {
+					$value = "null";
+				}
+
+				echo "{\"$key\":\"$value\"}";
+			}
+			echo "}";
+		}
+		
+		if(isset($_POST)) {
+#		else {
+#			echo json_encode($_POST);
+			echo "{\"Type\":\"[POST]\"";
+
+			foreach($_POST as $key => $value) {
+
+				if($value == null) {
+					$value = "null";
+				}
+				
+				echo "{\"$key\":\"$value\"}";
+			
+			}
+			echo "}";	
+		} 
+#			echo json_encode($_GET), "\n";
+#			echo json_encode($_POST);
+
 		
 		?>
 	</body>
